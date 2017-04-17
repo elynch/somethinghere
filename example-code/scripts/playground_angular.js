d5cee807583e873d1f1393d3e65d32afebee5b9c
ellenclynch@gmail.com <ellenlynch@ellens-mbp.attlocal.net>
2017-04-17 09:12:50 -0400

    Uploading example code stuff

---
+++ example-code/scripts/playground_angular.js
@@ -0,0 +1,11 @@
+var myAppModule=angular.module('myApp',[]);
+
+	myAppModule.controller('firstFormValidation',
+		function($scope){
+
+	});
+
+	myAppModule.controller('TestThisController',
+		function($scope){
+			$scope.youCheckedIt = true;
+	});
